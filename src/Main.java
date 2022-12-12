d32d530fa9738683426fed92ec6e6deb02e541cb
PioneerX7 <vita-kopy@yandex.ru>
2022-12-12 12:45:55 +0300

    Все задачи по циклам

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,18 @@ public class Main {
         task4();
         task5();
         task6();
+        task7();
+        task8();
+        task9();
+        task10();
+
     }
 
     public static void task1() {
         System.out.println("Задача 1");
 
         for (int i = 1; i <= 10; i++) {
-            System.out.print(i + " ");
+            System.out.println(i + " ");
         }
     }
 
@@ -20,7 +25,7 @@ public class Main {
         System.out.println("Задача 2");
 
         for (int i = 10; i > 0; i--) {
-            System.out.print(i + " ");
+            System.out.println(i + " ");
 
         }
     }
@@ -29,28 +34,74 @@ public class Main {
         System.out.println("Задача 3");
 
         for (int i = 0; i <= 17; i = i + 2) {
-            System.out.print(i + " ");
+            System.out.println(i + " ");
         }
     }
-    public static void task4(){
+
+    public static void task4() {
         System.out.println("Задача 4");
 
         for (int i = 10; i >= -10; i--) {
-            System.out.print(i + " ");
+            System.out.println(i + " ");
         }
     }
+
     public static void task5() {
-        System.out.println("Задача 4");
+        System.out.println("Задача 5");
 
         for (int i = 1904; i <= 2096; i = i + 4) {
             System.out.println(i + " год является високосным");
         }
     }
+
     public static void task6() {
-        System.out.println("Задача 4");
+        System.out.println("Задача 6");
 
         for (int i = 7; i <= 98; i = i + 7) {
-            System.out.print(i + " ");
+            System.out.println(i + " ");
+        }
+    }
+
+    public static void task7() {
+        System.out.println("Задача 7");
+
+        for (int i = 1; i <= 512; i = i * 2) {
+            System.out.println(i + " ");
+        }
+    }
+
+    public static void task8() {
+        System.out.println("Задача 8");
+
+        int moneyForMonth = 29000;
+        int total = 0;
+
+        for (int i = 1; i <= 12; i++) {
+            total = total + moneyForMonth;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + total + " рублей");
+        }
+    }
+
+    public static void task9() {
+        System.out.println("Задача 9");
+
+        int moneyForMonth = 29000;
+        int total = 0;
+
+        for (int i = 1; i <= 12; i++) {
+            total = total + total / 100;
+            total = total + moneyForMonth;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + total + " рублей");
+        }
+    }
+
+    public static void task10() {
+        System.out.println("Задача 10");
+        int x = 2;
+        int total = 0;
+        for (int i = 1; i <= 10; i++) {
+            total = i * x;
+            System.out.println(x + "*" + i + "=" + total);
         }
     }
 }
